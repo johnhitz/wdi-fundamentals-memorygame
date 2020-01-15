d4fef0ce4cc6eb9740646eccad0ea4a42138d39a
John Hitz <johnh@whodapet.com>
2020-01-14 23:11:16 -0600

    Added [Reset Cards] button and [Reset Game] button.
    [Reset Cards] flipps the cards over w/o resetting the score.
    
    [Reset Game] resets cards and score.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,10 +21,12 @@ const cards = [
     }
 ]
 var cardsInPlay = [];
+var score = 0;
 
 function checkForMatch() {
 	if(cardsInPlay[0] === cardsInPlay[1]) {
-		alert('You found a match!');
+		// alert('You found a match!');
+		score += 1;
 	} else {
 		alert("Sorry, try again!");
 	}
@@ -44,24 +46,47 @@ function flipCard() {
 
 function createBoard() {
 	for (let i = 0; i < cards.length; i++) {
-		let reset = document.getElementById('reset');
-		reset.addEventListener('click', resetCards);
+		// let reset = document.getElementById('reset-cards');
+		// reset.addEventListener('click', resetCards);
+
+		// let resetGame = document.getElementById('reset-game');
+		// resetGame.addEventListener('click', resetGame);
 
 		let cardElement = document.createElement('img');
 		cardElement.setAttribute('src', 'images/back.png');
 		cardElement.setAttribute('data-id', i);
 		cardElement.addEventListener('click', flipCard)
-
 		document.getElementById('game-board').appendChild(cardElement);
 	}
+	let reset = document.getElementById('reset-cards');
+	reset.addEventListener('click', resetCards);
+	let theScore = document.getElementById('score');
+
+	let resetTheGame = document.getElementById('reset-game');
+	resetTheGame.addEventListener('click', resetGame);
+	// console.log(score.innerHTML);
+	theScore.innerHTML = score;
 }
 
 function resetCards() {
-	for (let i = 0; i < cards.length; i++) {
-		let el = document.getElementsByTagName('img')[i];
-		el = el.setAttribute('src', 'images/back.png');
-		cardsInPlay = [];
-	}
+	// for (let i = 0; i < cards.length; i++) {
+	// 	let el = document.getElementsByTagName('img')[i];
+	// 	el = el.setAttribute('src', 'images/back.png');
+	// 	cardsInPlay = [];
+	// 	score = 0;
+	// }
+	game = document.getElementById('game-board');
+	game.innerHTML = "";
+	cardsInPlay = [];
+	createBoard();
+};
+
+function resetGame() {
+	console.log("WTF? Over!")
+	theScore = document.getElementById('score');
+	score = 0;
+	theScore.innerHTML = score;
+	resetCards();
 };
 
 createBoard();
