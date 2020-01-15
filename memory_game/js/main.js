990f3f2de1a62ec06f7b219bbfdb658ff1ada498
John Hitz <johnh@whodapet.com>
2020-01-14 20:50:18 -0600

    Added createBoard() and have game working!

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,7 +2,7 @@ const cards = [
     {
     	rank: "queen",
     	suit: "hearts",
-    	cardImage: "images/queen-of-hears.png"
+    	cardImage: "images/queen-of-hearts.png"
     },
     {
     	rank: "queen",
@@ -30,17 +30,26 @@ function checkForMatch() {
 	}
 };
 
-function flipCard(cardId) {
+function flipCard() {
+	cardId = this.getAttribute('data-id');
 	console.log("User flipped a " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 	if(cardsInPlay.length === 2) {
 		checkForMatch();
 	}
-	
-	// console.log(cardsInPlay);
 };
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard)
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
